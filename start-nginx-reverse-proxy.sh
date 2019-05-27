b6aa0bb46fd90f83d6fc4352b39322e1e76af7dc
Andreas Scheucher <andi@docker-host-01.mfg.otaya.cc>
2019-05-27 20:15:05 +0200

    init import

---
+++ start-nginx-reverse-proxy.sh
@@ -0,0 +1,9 @@
+/bin/bash
+
+set -e
+
+
+# for debugging:
+# docker run --mount type=bind,source="$(pwd)/nginx.conf",target=/etc/nginx/nginx.conf,readonly  --name tmp-nginx-container -p 80:80 -p 443:443 --rm nginx nginx-debug -g 'daemon off;'
+
+docker run --mount type=bind,source="$(pwd)/nginx.conf",target=/etc/nginx/nginx.conf,readonly  --name tmp-nginx-container -p 80:80 -p 443:443 --rm -d nginx
